2141b12c4b2b9e3d309d335a03f2bfb9bc1fa117
Your Name <you@example.com>
2025-05-07 18:37:19 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -58,19 +58,20 @@ int verificarConLista(int sudo[9][9], int i, int k) {
 
 int is_valid(Node* n){
   // Caso 1 y 2: Filas y columnas
-  int i = 0, k = 0;
-  if (!verificarConLista(n->sudo, i, k)) return 0;
-  if (!verificarConLista(n->sudo, k, i)) return 0;
+  int a = 0, b = 0;
+  if (!verificarConLista(n->sudo, a, b)) return 0;
+  if (!verificarConLista(n->sudo, b, a)) return 0;
 
   // Caso 3: Submatrices
-  /*
-  int k=0,p; 
+  int k=0,p;
   for(p=0;p<9;p++){
-    int listaSub[10] = {};
+    int listaSub[10] = {0};
     int i=3*(k/3) + (p/3) ;
     int j=3*(k%3) + (p%3) ;
+    if (!n->sudo[i][j]) continue;
+    if (listaSub[n->sudo[i][j]]) return 0;
     
-    if (es_repetido(listaSub, n->sudo[i][j])) return 0;
+    listaSub[n->sudo[i][j]] = 1;
     if(p%3 == 2) k++;
   }*/
   return 1;
