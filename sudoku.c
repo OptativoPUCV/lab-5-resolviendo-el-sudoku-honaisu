17ed299aa487468917362e8e2a9bd71447bd7016
Your Name <you@example.com>
2025-05-07 18:23:19 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -60,7 +60,7 @@ int is_valid(Node* n){
       if (listaFila[n->sudo[i][k]]) return 0;
 
       listaFila[n->sudo[i][k]] = 1;
-      printf("%d ", listaFila[n->sudo[i][k]]);
+      printf("%d ", n->sudo[i][k]);
       //listaColumna[n->sudo[k][i]] = 1;
       
     } while((k++)<9);
