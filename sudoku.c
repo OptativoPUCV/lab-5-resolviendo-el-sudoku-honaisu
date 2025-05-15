19563dc6887d8cddafdee3ccf2d6b38f0b431986
Your Name <you@example.com>
2025-05-14 20:23:37 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -43,36 +43,21 @@ void print_node(Node* n){
     printf("\n");
 }
 
-int verificarConLista(int sudo[9][9], int i, int k) {
-  for (i = 0, k = 0; i < 9; i++, k = 0) {
-    int lista[10] = {0} ;
-    do {
-      if (!sudo[i][k]) continue;
-      if (lista[sudo[i][k]]) return 0;
-
-      lista[sudo[i][k]] = 1;
-    } while((k++)<8);
-  }
-  return 1;
-}
-
 int is_valid(Node* n){
   // Caso 1 y 2: Filas y columnas
   int a = 0, b = 0;
-  if (!verificarConLista(n->sudo, a, b)) return 0;
-  if (!verificarConLista(n->sudo, b, a)) return 0;
-
+  
   // Caso 3: Submatrices
-  int k=0,p;
-  for(p=0;p<9;p++){
+  int k = 0, p;
+  for(p = 0; p < 9; p++) {
     int listaSub[10] = {0};
-    int i=3*(k/3) + (p/3) ;
-    int j=3*(k%3) + (p%3) ;
+    int i = 3 * (k / 3) + (p / 3);
+    int j = 3 * (k % 3) + (p % 3);
     if (!n->sudo[i][j]) continue;
     if (listaSub[n->sudo[i][j]]) return 0;
     
     listaSub[n->sudo[i][j]] = 1;
-    if(p%3 == 2) k++;
+    if(p % 3 == 2) k++;
   }
   return 1;
 }
