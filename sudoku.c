30fcd839ab62fdd69419ddaf53f3d7d84d270d1c
Your Name <you@example.com>
2025-05-14 20:37:36 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -63,15 +63,15 @@ int is_valid(Node* n){
   if (!verificarConLista(n->sudo, b, a)) return 0;
 
   // Caso 3: Submatrices
-  int k = 2;
-  for(int p = 0; p < 9; p++) {
+  int k = 0;
+  for(int p = 0; p < 9, k < 9; p++) {
     int listaSub[10] = {0};
     int i = 3 * (k / 3) + (p / 3);
     int j = 3 * (k % 3) + (p % 3);
     if (listaSub[n->sudo[i][j]]) return 0;
     printf("%d ", n->sudo[i][j]);
     listaSub[n->sudo[i][j]] = 1;
-    if(p % 3 == 2) putchar('\n');
+    if(p % 3 == 2) { putchar('\n'); k++; }
   }
   return 1;
 }
