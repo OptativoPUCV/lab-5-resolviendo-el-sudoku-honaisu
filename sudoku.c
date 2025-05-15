1d7a79e8388e8bef2300f1d6cda94deaf8e72b52
Your Name <you@example.com>
2025-05-14 21:13:15 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -75,7 +75,7 @@ int verificar_columnas(const int sudo[9][9]) {
 
 int verificar_submatrices(const int sudo[9][9]) {
   int i, j, casilla;
-  for (int k = 0; k < 3; k++) {
+  for (int k = 0; k < 3; ) {
     int lista[10] = {0};
     for (int p = 0; p < 9; p++) {
       i = 3 * (k / 3) + (p / 3);
@@ -83,7 +83,7 @@ int verificar_submatrices(const int sudo[9][9]) {
       
       casilla = sudo[i][j];
       printf("%d ", casilla);
-      if (p % 3 == 2) break;
+      if (p % 3 == 2) { k++; break; }
       if (lista[casilla]) return 0;
       if (!casilla) continue;
       
